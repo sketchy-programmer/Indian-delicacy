492e40bbf6b70a262c6433dec2d7c14b942b1891
sketchy-programmer <paramvir.s.study@gmail.com>
2024-10-09 21:18:28 -0400

    create the footer component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import RestaurentStory from './components/RestaurentStory.js';
 import IngredientsSection from './components/Ingredients.js';
 import WordsFromChef from './components/Chef.js';
 import CustomerReview from './components/CustomerReview.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <IngredientsSection />
       <WordsFromChef />
       <CustomerReview />
+      <Footer />
     </div>
     </>
   );
